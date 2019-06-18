ec3acbff50236aca559b7b14d328d4e5a81faa79
Joshua Hunter <50775251+hunterhanna2010@users.noreply.github.com>
2019-06-18 12:29:14 -0700

    adding game logic
    GA Memory Game exercise

---
+++ js/main.js
@@ -0,0 +1,82 @@
+
+
+var cards = [
+{
+rank: "queen",
+suit: "hearts",
+cardImage: "images/queen-of-hearts.png",
+},
+{
+rank: "queen",
+suit: "diamonds",
+cardImage: "images/queen-of-diamonds.png",
+},
+{
+rank: "king",
+suit: "hearts",
+cardImage: "images/king-of-hearts.png",
+},
+{
+rank: "king",
+suit: "diamonds",
+cardImage: "images/king-of-diamonds.png",
+}
+];
+
+var cardsInPlay = [];
+var playerScore = 0;
+var scoreDisplay = document.getElementById('scoreDisplay')
+
+var checkForMatch = function () {
+
+	if (cardsInPlay.length === 2) {
+	if (cardsInPlay [0] === cardsInPlay [1]) {
+		alert("You found a match!");
+		playerScore++;
+		scoreDisplay.textContent = playerScore;
+	} else {
+		alert("Sorry, try again.");
+	}
+}
+}
+
+var flipCard = function () {
+
+var cardId = this.getAttribute("data-id");
+this.setAttribute("src", cards[cardId].cardImage);
+//console.log("User flipped " + cards[cardId].rank);
+
+cardsInPlay.push(cards[cardId].rank);
+
+ if (cardsInPlay.length === 2) {
+	checkForMatch ();
+} else {
+	flipCard ();
+}
+}
+
+var createBoard = function() {
+	for (var i = 0; i < cards.length; i++){
+
+	var cardElement = document.createElement("img");
+	cardElement.setAttribute("src", "images/back.png");
+	cardElement.setAttribute("data-id", i);
+	cardElement.addEventListener("click", flipCard);
+	document.getElementById("game-board").appendChild(cardElement);
+}
+}
+
+var resetButton = document.getElementById('reset');
+	resetButton.addEventListener("click", function() {
+	var playedCards = document.getElementById("game-board").children
+	for (var i = 0; i < playedCards.length; i++) {
+	playedCards[i].setAttribute("src", "images/back.png");
+	cardsInPlay = [];	
+	}
+
+});
+
+ 
+createBoard();
+
+
